800ed0e2380880e0e0e044fe83cd6533d478ce56
quanmh25 <maihoangquan250205@gmail.com>
2024-10-13 23:09:41 +0300

    new

--- test.py
+++ test.py
@@ -1,28 +1,19 @@
-from math import *
+from time import sleep
+from os import system
 
-red = "\x1b[48;5;196m"
-green = "\x1b[48;5;28m"
-reset_color = "\x1b[0m"
+WHITE = "\033[47m"
+BLACK = "\033[40m"
+i = 0
+frames = ["|", "\\", "-", "/"]
 
-# Open the file and read the data
-try:
-    with open("sequence.txt") as file:
-        a = [abs(float(file.readline())) for _ in range(250)]
-except FileNotFoundError:
-    print("The file 'sequence.txt' was not found.")
-    exit()
-except ValueError:
-    print("There was an error reading numbers from the file.")
-    exit()
+if __name__ == "__main__":
+    for i in range(300):
+        f = i%4
+        i = i//15
+        download = f"Hacking Pentagon: {i*5}%" + "[" + WHITE + " "*(i) + BLACK + " "*(20-i) + "]" + BLACK + " "
+        print(frames[f] + "  " + download)
+        sleep(0.05)
+        print("\033[H")
 
-# Calculate sums and percentages
-s1, s2 = sum(a[:125]), sum(a[125:])
-s = s1 + s2
-if s == 0:
-    print("The sum of the numbers is zero; cannot calculate percentages.")
-else:
-    per_1, per_2 = s1 / s, s2 / s 
 
-    # Print colored blocks
-    print(red + ' ' * int(per_1 * 100) + reset_color)
-    print(green + ' ' * int(per_2 * 100) + reset_color)
+        
